e11a9094d08df7114d6ba1427a588194a4a24291
sdps0 <ilyasgrigorev@gmail.com>
2026-07-15 10:05:50 +0400

    Добавлены активации, награды и инвентарь

--- stp_database/repo/Achievements/__init__.py
+++ stp_database/repo/Achievements/__init__.py
@@ -1,9 +1,15 @@
 ﻿"""Репозитории Achievements."""
 
 from stp_database.repo.Achievements.achievements import AchievementsRepo
+from stp_database.repo.Achievements.activations import ActivationsRepo
+from stp_database.repo.Achievements.awards import AwardsRepo
+from stp_database.repo.Achievements.inventory import InventoryRepo
 from stp_database.repo.Achievements.log_achievements import LogAchievementsRepo
 
 __all__ = [
     "AchievementsRepo",
+    "ActivationsRepo",
+    "AwardsRepo",
+    "InventoryRepo",
     "LogAchievementsRepo",
 ]
